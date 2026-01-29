522c612102289b0c2d8b9bbf5803716530acc50a
PollegGames <rexnez@gmail.com>
2026-01-29 10:15:38 +0100

    clean scripts unused

--- Assets/Scripts/Interfaces/IPOIReservationService.cs
+++
@@ -1,7 +0,0 @@
-public interface IPOIReservationService
-{
-    RoomWaypoint GetWorkOrRestPoint(RoomWaypoint exclude = null);
-    RoomWaypoint GetFirstRestPoint(RoomWaypoint exclude = null);
-    RoomWaypoint GetFirstFreeSecurityPoint();
-    void ReleasePOI(RoomWaypoint poi);
-}
